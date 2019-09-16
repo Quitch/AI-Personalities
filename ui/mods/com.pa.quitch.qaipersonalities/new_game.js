7b07ff50c0505e898b01bb2b6065c66f0f756b1c
Quitch <Quitch@users.noreply.github.com>
2019-09-16 12:06:26 +0100

    Match personality casing

--- ui/mods/com.pa.quitch.qaipersonalities/new_game.js
+++ ui/mods/com.pa.quitch.qaipersonalities/new_game.js
@@ -15,7 +15,7 @@ function qaipersonalities() {
     "AIP-Absurd Aggressive": {
       display_name: "!LOC:Absurd Aggressive",
       neural_data_mod: 1.33,
-      personality_tags: ["vanilla", "PreventsWaste"]
+      personality_tags: ["Vanilla", "PreventsWaste"]
     },
     "AIP-Absurd Air": {
       display_name: "!LOC:Absurd Air",
@@ -24,7 +24,7 @@ function qaipersonalities() {
       percent_air: 1,
       percent_naval: 0,
       percent_orbital: 0,
-      personality_tags: ["vanilla", "PreventsWaste"]
+      personality_tags: ["Vanilla", "PreventsWaste"]
     },
     "AIP-Absurd Bot": {
       display_name: "!LOC:Absurd Bot",
@@ -32,20 +32,20 @@ function qaipersonalities() {
       percent_bot: 0.95,
       percent_naval: 0,
       percent_orbital: 0.05,
-      personality_tags: ["vanilla", "PreventsWaste"]
+      personality_tags: ["Vanilla", "PreventsWaste"]
     },
     "AIP-Absurd Cautious": {
       display_name: "!LOC:Absurd Cautious",
       neural_data_mod: 0.75,
       min_basic_fabbers: 4,
-      personality_tags: ["vanilla", "PreventsWaste"]
+      personality_tags: ["Vanilla", "PreventsWaste"]
     },
     "AIP-Absurd Fast Tech": {
       display_name: "!LOC:Absurd Fast Tech",
       adv_eco_mod: 0,
       adv_eco_mod_alone: 0,
       min_basic_fabbers: 4,
-      personality_tags: ["vanilla", "PreventsWaste"]
+      personality_tags: ["Vanilla", "PreventsWaste"]
     },
     "AIP-Absurd Galactic War": {
       display_name: "!LOC:Absurd Galactic War",
@@ -54,7 +54,7 @@ function qaipersonalities() {
       min_basic_fabbers: 1,
       max_basic_fabbers: 15,
       max_advanced_fabbers: 50,
-      personality_tags: ["vanilla", "PreventsWaste"],
+      personality_tags: ["Vanilla", "PreventsWaste"],
       metalDrainCheck: 0.45,
       metalDemandCheck: 0.562,
       energyDrainCheck: 0.62,
@@ -67,13 +67,13 @@ function qaipersonalities() {
       percent_air: 0,
       percent_naval: 0,
       percent_orbital: 0,
-      personality_tags: ["vanilla", "PreventsWaste"]
+      personality_tags: ["Vanilla", "PreventsWaste"]
     },
     "AIP-Absurd Low Tech": {
       display_name: "!LOC:Absurd Low Tech",
       adv_eco_mod: 3,
       adv_eco_mod_alone: 2,
-      personality_tags: ["vanilla", "PreventsWaste"]
+      personality_tags: ["Vanilla", "PreventsWaste"]
     },
     "AIP-Absurd Naval": {
       display_name: "!LOC:Absurd Naval",
@@ -82,7 +82,7 @@ function qaipersonalities() {
       percent_air: 0.2,
       percent_naval: 0.8,
       percent_orbital: 0,
-      personality_tags: ["vanilla", "PreventsWaste"]
+      personality_tags: ["Vanilla", "PreventsWaste"]
     },
     "AIP-Absurd Orbital": {
       display_name: "!LOC:Absurd Orbital",
@@ -91,7 +91,7 @@ function qaipersonalities() {
       percent_air: 0.05,
       percent_naval: 0,
       percent_orbital: 0.775,
-      personality_tags: ["vanilla", "PreventsWaste"]
+      personality_tags: ["Vanilla", "PreventsWaste"]
     },
     "AIP-Absurd Rush": {
       display_name: "!LOC:Absurd Rush",
@@ -102,7 +102,7 @@ function qaipersonalities() {
       percent_orbital: 0,
       neural_data_mod: 1.33,
       adv_eco_mod: 3,
-      personality_tags: ["vanilla", "PreventsWaste"]
+      personality_tags: ["Vanilla", "PreventsWaste"]
     },
     "AIP-Absurd Tank": {
       display_name: "!LOC:Absurd Tank",
@@ -111,7 +111,7 @@ function qaipersonalities() {
       percent_air: 0,
       percent_naval: 0,
       percent_orbital: 0.05,
-      personality_tags: ["vanilla", "PreventsWaste"]
+      personality_tags: ["Vanilla", "PreventsWaste"]
     },
     "AIP-Absurd Turtle": {
       display_name: "!LOC:Absurd Turtle",
@@ -124,7 +124,7 @@ function qaipersonalities() {
       min_basic_fabbers: 4,
       max_basic_fabbers: 60,
       max_advanced_fabbers: 60,
-      personality_tags: ["vanilla", "PreventsWaste"]
+      personality_tags: ["Vanilla", "PreventsWaste"]
     }
   };
 
