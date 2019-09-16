c7ac94e46e93c532663e58231695e0f3f185f51a
Quitch <Quitch@users.noreply.github.com>
2019-09-16 10:39:07 +0100

    Revert assign to extend

--- ui/mods/com.pa.quitch.qaipersonalities/new_game.js
+++ ui/mods/com.pa.quitch.qaipersonalities/new_game.js
@@ -131,12 +131,12 @@ function qaipersonalities() {
   var baseline = aiPersonalities.Absurd;
 
   newPersonalities = _.mapValues(newPersonalities, function(personality, name) {
-    var result = _.assign(_.clone(baseline), personality);
+    var result = _.extend(_.clone(baseline), personality);
     result["name"] = name;
     return result;
   });
 
-  _.assign(aiPersonalities, newPersonalities);
+  _.extend(aiPersonalities, newPersonalities);
 
   model.aiPersonalities.valueHasMutated();
 }
